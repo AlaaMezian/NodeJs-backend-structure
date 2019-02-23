6d41bf1a02b3d0917f4953f0b29c65ac1949f07c
Ala'a <alaa.mezian.mail@gmail.com>
2019-02-23 23:44:24 +0200

    Update index.js

--- router/api/index.js
+++ router/api/index.js
@@ -4,10 +4,6 @@ const router = require('express').Router();
 
 router.use('/users', require('./usersRouter'));
 
-router.use('/termsAndCondition', require('./termsAndConditionRouter'));
-
-router.use('/version', require('./versionRouter'));
-
 router.use('/email', require('./sendEmail'));
 
 router.use('/', require('./authRouter'));
